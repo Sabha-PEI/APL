f41f75654fff8f8840f4ad78e0c8b346fe4afeaf
Arpit Dalal <arpitdalalm@gmail.com>
2024-05-31 12:31:18 -0400

    fix: remove iframe and add manual data

--- app/routes/admin+/index.tsx
+++ app/routes/admin+/index.tsx
@@ -1,7 +1,16 @@
 import { type HeadersFunction, type LoaderFunctionArgs } from '@remix-run/node'
 import { Form, useLoaderData } from '@remix-run/react'
 import { Button } from '../../components/ui/button'
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from '../../components/ui/table'
 import { requirePlayerId } from '../../utils/auth.server'
+import { prisma } from '../../utils/db.server'
 import { getIframeSrc } from './index.server'
 
 export const headers: HeadersFunction = () => {
@@ -13,12 +22,35 @@ export const headers: HeadersFunction = () => {
 
 export async function loader({ request }: LoaderFunctionArgs) {
 	await requirePlayerId(request)
+	const players = await prisma.player.findMany({
+		select: {
+			firstName: true,
+			lastName: true,
+			address: true,
+			phNo: true,
+			dob: true,
+			healthCard: true,
+			playingRole: true,
+			tshirtSize: true,
+			batsmanRating: true,
+			handedBatsman: true,
+			battingComment: true,
+			bowlerRating: true,
+			armBowler: true,
+			typeBowler: true,
+			bowlingComment: true,
+			fielderRating: true,
+			fielderComment: true,
+			paid: true,
+			paidAt: true,
+		},
+	})
 
-	return { iframeSrc: getIframeSrc() }
+	return { iframeSrc: getIframeSrc(), players }
 }
 
 export default function Index() {
-	const { iframeSrc } = useLoaderData<typeof loader>()
+	const { players } = useLoaderData<typeof loader>()
 
 	return (
 		<main className="mt-128 container flex flex-1 flex-col place-items-center">
@@ -29,14 +61,70 @@ export default function Index() {
 			</Form>
 
 			<div className="mt-5 flex w-full flex-1 flex-col pb-20">
-				<iframe
+				{/* <iframe
 					title="Prisma Studio"
 					src={iframeSrc}
 					width="100%"
 					height="100%"
 					className="flex-1"
 					allowFullScreen
-				></iframe>
+				></iframe> */}
+				<p className="text-center">{players.length} players registered</p>
+				<Table className="mt-5 border">
+					<TableHeader>
+						<TableRow className="divide-x">
+							<TableHead></TableHead>
+							<TableHead>Paid</TableHead>
+							<TableHead>Paid At</TableHead>
+							<TableHead>First name</TableHead>
+							<TableHead>Last name</TableHead>
+							<TableHead>Address</TableHead>
+							<TableHead>Phone</TableHead>
+							<TableHead>DOB</TableHead>
+							<TableHead>Health card</TableHead>
+							<TableHead>Playing role</TableHead>
+							<TableHead>T-shirt size</TableHead>
+							<TableHead>Batsman rating</TableHead>
+							<TableHead>Handed batsman</TableHead>
+							<TableHead>Batting comment</TableHead>
+							<TableHead>Bowler rating</TableHead>
+							<TableHead>Arm bowler</TableHead>
+							<TableHead>Type bowler</TableHead>
+							<TableHead>Bowling comment</TableHead>
+							<TableHead>Fielding rating</TableHead>
+							<TableHead>Fielding comment</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{players.map((player, index) => (
+							<TableRow
+								key={`${player.firstName}-${player.dob}`}
+								className="divide-x"
+							>
+								<TableCell>{++index}</TableCell>
+								<TableCell>{player.paid ? 'Yes' : 'No'}</TableCell>
+								<TableCell>{player.paidAt}</TableCell>
+								<TableCell>{player.firstName}</TableCell>
+								<TableCell>{player.lastName}</TableCell>
+								<TableCell>{player.address}</TableCell>
+								<TableCell>{player.phNo}</TableCell>
+								<TableCell>{player.dob.toString().split('T')[0]}</TableCell>
+								<TableCell>{player.healthCard ? 'Yes' : 'No'}</TableCell>
+								<TableCell>{player.playingRole}</TableCell>
+								<TableCell>{player.tshirtSize}</TableCell>
+								<TableCell>{player.batsmanRating}</TableCell>
+								<TableCell>{player.handedBatsman}</TableCell>
+								<TableCell>{player.battingComment}</TableCell>
+								<TableCell>{player.bowlerRating}</TableCell>
+								<TableCell>{player.armBowler}</TableCell>
+								<TableCell>{player.typeBowler}</TableCell>
+								<TableCell>{player.bowlingComment}</TableCell>
+								<TableCell>{player.fielderRating}</TableCell>
+								<TableCell>{player.fielderComment}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
 			</div>
 		</main>
 	)
