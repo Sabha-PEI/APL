712a865fdc61d452e0f0ebcf0339c9fef61eb0fb
Arpit Dalal <arpitdalalm@gmail.com>
2024-06-03 14:07:00 -0400

    feat: add teams

---
+++ app/routes/admin+/teams.tsx
@@ -0,0 +1,61 @@
+import { type LoaderFunctionArgs, type SerializeFrom } from '@remix-run/node'
+import { Link, useLoaderData } from '@remix-run/react'
+import { requirePlayerId } from '../../utils/auth.server'
+import { prisma } from '../../utils/db.server'
+import { getTeamImgSrc } from '../../utils/misc'
+
+export async function loader({ request }: LoaderFunctionArgs) {
+	await requirePlayerId(request)
+
+	const teams = await prisma.team.findMany({
+		select: {
+			id: true,
+			name: true,
+			image: {
+				select: {
+					id: true,
+				},
+			},
+		},
+	})
+
+	return { teams }
+}
+
+export default function Teams() {
+	const { teams } = useLoaderData<typeof loader>()
+
+	return (
+		<main className="container flex flex-1 flex-col place-items-center">
+			<h1 className="text-4xl font-bold">Teams</h1>
+			<Link to="/admin/teams/new" className="mt-3 hover:underline">
+				Create Team
+			</Link>
+			<ul className="mt-5 flex flex-wrap gap-5">
+				{teams.map(team => (
+					<Team key={team.id} team={team} />
+				))}
+			</ul>
+		</main>
+	)
+}
+
+type TeamProps = {
+	team: SerializeFrom<typeof loader>['teams'][number]
+}
+function Team({ team }: TeamProps) {
+	const teamImgSrc = getTeamImgSrc(team.image.id)
+
+	return (
+		<li className="rounded-lg border border-accent-foreground/40">
+			<img
+				src={teamImgSrc}
+				alt={team.name}
+				className="mx-auto aspect-auto size-32"
+			/>
+			<div className="flex flex-col items-center justify-center gap-3 px-3 py-2">
+				<h2>{team.name}</h2>
+			</div>
+		</li>
+	)
+}
---
+++ app/routes/admin+/teams_.new.tsx
@@ -0,0 +1,62 @@
+import {
+	type LoaderFunctionArgs,
+	type ActionFunctionArgs,
+	unstable_createMemoryUploadHandler,
+	unstable_parseMultipartFormData,
+	redirect,
+} from '@remix-run/node'
+import { Form } from '@remix-run/react'
+import { Button } from '../../components/ui/button'
+import { Input } from '../../components/ui/input'
+import { requirePlayerId } from '../../utils/auth.server'
+import { prisma } from '../../utils/db.server'
+
+export async function loader({ request }: LoaderFunctionArgs) {
+	await requirePlayerId(request)
+	return {}
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+	await requirePlayerId(request)
+	const uploadHandler = unstable_createMemoryUploadHandler({
+		maxPartSize: 500_000,
+	})
+
+	const formData = await unstable_parseMultipartFormData(request, uploadHandler)
+	const name = formData.get('name') as string
+	const image = formData.get('image') as File
+
+	const team = await prisma.team.create({
+		data: {
+			name,
+			image: {
+				create: {
+					contentType: image.type,
+					blob: Buffer.from(await image.arrayBuffer()),
+				},
+			},
+		},
+	})
+
+	if (!team) {
+		throw new Error('Could not create team')
+	}
+
+	return redirect(`/admin/teams`)
+}
+
+export default function TeamsNew() {
+	return (
+		<main className="container flex flex-1 flex-col place-items-center">
+			<Form
+				method="POST"
+				encType="multipart/form-data"
+				className="flex flex-col gap-3"
+			>
+				<Input type="text" name="name" placeholder="Team Name" />
+				<Input type="file" name="image" />
+				<Button type="submit">Create Team</Button>
+			</Form>
+		</main>
+	)
+}
